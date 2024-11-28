d980dd83f8c9ecffaf100ff1824c16b90e379315
Denidu Gamage <denidugamage21@gmail.com>
2024-11-28 11:37:08 +0530

    Initial Commit

---
+++ MapKit-SwiftUI/MapKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_SwiftUIApp.swift
+//  MapKit-SwiftUI
+//
+//  Created by Denidu Gamage on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapKit-SwiftUITests/MapKit_SwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  MapKit_SwiftUITests.swift
+//  MapKit-SwiftUITests
+//
+//  Created by Denidu Gamage on 2024-11-28.
+//
+
+import Testing
+
+struct MapKit_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
